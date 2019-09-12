11083286e01ced24dd3e5373a63b060a00110c33
Yuriy Kostin <kostin.yurij@gmail.com>
2019-09-12 15:28:28 +0300

    Update build

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,10 +1,9 @@
-import gulp from 'gulp';
-import babel from 'gulp-babel';
-import sourcemaps from 'gulp-sourcemaps';
-import eslint from 'gulp-eslint';
-import del from 'del';
-// import concat from 'gulp-concat';
-import nodemon from 'gulp-nodemon';
+import gulp from "gulp";
+import babel from "gulp-babel";
+import sourcemaps from "gulp-sourcemaps";
+import eslint from "gulp-eslint";
+import del from "del";
+import nodemon from "gulp-nodemon";
 
 gulp.task('clean-build', function(cb) {
   return del(['dist'], cb);
@@ -18,7 +17,6 @@ gulp.task('compile', function() {
     .pipe(eslint.failAfterError())
     .pipe(sourcemaps.init())
     .pipe(babel())
-    // .pipe(concat('app.js'))
     .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest('dist'));
 });
