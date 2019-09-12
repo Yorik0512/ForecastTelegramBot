bbf3f2a1e749838f6e0c69e07fb6d1e0900ebb2d
Yuriy Kostin <kostin.yurij@gmail.com>
2019-09-12 15:22:15 +0300

    Update build

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,29 +1,36 @@
-import gulp from "gulp";
-import babel from "gulp-babel";
-import sourcemaps from "gulp-sourcemaps";
-import eslint from "gulp-eslint";
-// import concat from "gulp-concat";
-import nodemon from "gulp-nodemon";
+import gulp from 'gulp';
+import babel from 'gulp-babel';
+import sourcemaps from 'gulp-sourcemaps';
+import eslint from 'gulp-eslint';
+import del from 'del';
+// import concat from 'gulp-concat';
+import nodemon from 'gulp-nodemon';
 
-gulp.task("compile", function() {
+gulp.task('clean-build', function(cb) {
+  return del(['dist'], cb);
+});
+
+gulp.task('compile', function() {
   return gulp
-    .src("src/**/*.js")
+    .src('src/**/*.js')
     .pipe(eslint())
     .pipe(eslint.format())
     .pipe(eslint.failAfterError())
     .pipe(sourcemaps.init())
     .pipe(babel())
-    // .pipe(concat("app.js"))
-    .pipe(sourcemaps.write("."))
-    .pipe(gulp.dest("dist"));
+    // .pipe(concat('app.js'))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('dist'));
 });
 
-gulp.task("watch", function(done) {
+gulp.task('build', gulp.series('clean-build', 'compile'));
+
+gulp.task('watch', function(done) {
   nodemon({
-    script: "./dist/index.js",
-    ext: "js",
-    tasks: ["compile"],
-    watch: "./src/",
+    script: './dist/index.js',
+    ext: 'js',
+    tasks: ['compile'],
+    watch: './src/',
     done: done
   });
 });
