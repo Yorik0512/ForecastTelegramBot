d23ab6021b1e92cc478bc3ef75d7b0ac2358fbf7
Yuriy Kostin <kostin.yurij@gmail.com>
2019-09-12 14:29:41 +0300

    Remove dist folder

--- dist/entites/main.entites.js
+++
@@ -1,7 +0,0 @@
-// export default  class MainEntities {
-//   hello(userName) {
-//     return `Приветики @${userName}`;
-//   }
-// }
-"use strict";
-//# sourceMappingURL=main.entites.js.map
--- dist/services/wind-direction.js
+++
@@ -1,2 +0,0 @@
-"use strict";
-//# sourceMappingURL=wind-direction.js.map
